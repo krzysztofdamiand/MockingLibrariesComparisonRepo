4e8f115629bf28aa48e9569e79c8827d9383fd56
krzysztofdamiand <k.dabrowski@rector.com.pl>
2015-12-02 17:19:16 +0100

    Master Project
    blablabla

---
+++ MockingLibrariesComparison/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MockingLibrariesComparison.Program;
+
+namespace MockingLibrariesComparison
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
